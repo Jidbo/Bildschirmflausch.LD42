f02bf7d00bee26bd82497ad4ac5b3c134362700d
Triceraptodactyl <reispfannenfresser@gmail.com>
2018-08-11 14:40:23 +0200

    added prefabs
    added Shelf
    added smallShelf
    added crafting for the storage pallet

--- Assets/Scripts/Storages/StoragePallet.cs
+++ Assets/Scripts/Storages/StoragePallet.cs
@@ -3,13 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class StoragePallet : Storage {
+    [SerializeField]
+    GameObject shelf;
+
     protected override bool CanStore(GameObject go) {
         return go.CompareTag("crate");
     }
 
     protected override void OnObjectAdded(GameObject go) {
         if (IsFull()) {
-            //TODO create shelf in current position
+            GameObject newShelf = UnityEngine.Object.Instantiate(shelf);
+            newShelf.transform.position = transform.position;
+            Destroy(gameObject);
         }
     }
 }
