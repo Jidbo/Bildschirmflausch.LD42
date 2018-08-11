c2d122fdcde93c8bc3d6a1113098111b9676623d
Jidbo <Jidbo@users.noreply.github.com>
2018-08-11 17:04:09 +0200

    new player controlls

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 using System;
 
 public class PlayerMovement : MonoBehaviour {
-    [SerializeField]
-    float speed = 100;
+    //[SerializeField]
+    float rotationSpeed = 150;
+	//[SerializeField]
+    float movementSpeed = 10F;
     [SerializeField]
     float acceleration = 0.9f;
 
     Rigidbody rb;
-    float xSpeed;
+    float forwardSpeed;
     float zSpeed;
     float rotationAngle;
     Transform t;
@@ -23,15 +25,17 @@ public class PlayerMovement : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-        xSpeed = Input.GetAxis("Horizontal") * speed;
-        zSpeed = Input.GetAxis("Vertical") * speed;
-		rotationAngle+=0.1f;
+        zSpeed = Input.GetAxis("Horizontal") * Time.deltaTime * rotationSpeed;
+        forwardSpeed = Input.GetAxis("Vertical") * Time.deltaTime * movementSpeed;
+
+        transform.Rotate(0, zSpeed, 0);
+        transform.Translate(forwardSpeed, 0, 0);
     }
 
-    private void FixedUpdate() {
-		rb.velocity = rb.velocity * (1 - acceleration) + acceleration * new Vector3(xSpeed, rb.velocity.y, zSpeed);
+  //  private void FixedUpdate() {
+		//rb.velocity = rb.velocity * (1 - acceleration) + acceleration * new Vector3(xSpeed, rb.velocity.y, zSpeed);
 
-        if(rb.velocity.magnitude >= 0.1)
-			t.LookAt(t.position + new Vector3((float)System.Math.Sin(rotationAngle), 0, (float)System.Math.Cos(rotationAngle)));
-    }
+   //     if(rb.velocity.magnitude >= 0.1)
+			//t.LookAt(t.position + new Vector3((float)System.Math.Sin(rotationAngle), 0, (float)System.Math.Cos(rotationAngle)));
+    //}
 }
