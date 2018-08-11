ec15d233dac2e91440a07966c9b94d745ec8c160
Jidbo <Jidbo@users.noreply.github.com>
2018-08-12 00:06:09 +0200

    add basic Main Menu

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIController : MonoBehaviour {
+        
+    public void QuitGame() {
+        Debug.Log("Quit Game");
+        Application.Quit();
+    }
+
+
+    public void LoadSceneByIndex(int index) {
+        Debug.Log("Load Scene: " + index);
+        SceneManager.LoadScene(index);
+    }
+}
