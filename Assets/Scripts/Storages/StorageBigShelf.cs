4e94d1b0585d9490cd6a2a2647d3e1919ef20547
Triceraptodactyl <reispfannenfresser@gmail.com>
2018-08-11 21:24:37 +0200

    many small changes
    changed collision detection
    waste can spawn now
    waste can be picked up
    big shelves can now store planks and crates

--- Assets/Scripts/Storages/StorageBigShelf.cs
+++ Assets/Scripts/Storages/StorageBigShelf.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class StorageBigShelf : Storage {
-    public StorageBigShelf() : base(new string[] { "shelf", "waste" }, 4) {
+    public StorageBigShelf() : base(new string[] { "shelf", "waste", "pallet", "chest"}, 4) {
 
     }
 
