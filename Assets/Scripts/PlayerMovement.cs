8fa6f69ee09d10837b012e0190c90a59f8a4eba3
Triceraptodactyl <reispfannenfresser@gmail.com>
2018-08-11 10:58:07 +0200

    Sample Scene stuff + Player Movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    [SerializeField]
+    float speed = 100;
+
+    Rigidbody rb;
+    float xSpeed;
+    float zSpeed;
+    float rotationAngle;
+    Transform t;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        t = GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        xSpeed = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+        zSpeed = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+        
+    }
+
+    private void FixedUpdate() {
+        rb.velocity = new Vector3(xSpeed, 0, zSpeed);
+        t.LookAt(t.position + rb.velocity);
+    }
+}
