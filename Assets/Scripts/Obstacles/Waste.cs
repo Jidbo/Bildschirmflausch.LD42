66d7717a082be836ef5ea7cfcda3b12372748ac6
Triceraptodactyl <reispfannenfresser@gmail.com>
2018-08-12 17:57:46 +0200

    Added some more Ramps and spawners

---
+++ Assets/Scripts/Obstacles/Waste.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waste : MonoBehaviour {
+
+    private bool collided;
+    [SerializeField]
+    float maxTimeTillBoom;
+    [SerializeField]
+    float regenerationAmount;
+    [SerializeField]
+    float currentTimeTillBoom;
+    
+    private void Start() {
+        currentTimeTillBoom = maxTimeTillBoom;
+	}
+
+    private void OnCollisionStay(Collision collision) {
+        if (currentTimeTillBoom > 0 && collision.gameObject.CompareTag("waste")) {
+            currentTimeTillBoom -= Time.deltaTime;
+        }
+    }
+
+    private void FixedUpdate() {
+        if (currentTimeTillBoom <= 0) {
+            Explode();
+            return;
+        }
+        if (currentTimeTillBoom < maxTimeTillBoom) {
+            currentTimeTillBoom += regenerationAmount * Time.deltaTime;
+        }
+    }
+
+    private void Explode() {
+        Destroy(gameObject);
+    }
+}
