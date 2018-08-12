12d3feec02343ac2d5575cce95a1f7e18d623171
Chris <christian.seybert@stud.tu-darmstadt.de>
2018-08-12 19:23:35 +0200

    Camera is behind the player

---
+++ Assets/Scripts/Player/Camera.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour {
+
+    public GameObject player;
+    public float damping = 1;
+    Vector3 offset;
+
+    void Start()
+    {
+        offset = player.transform.position - transform.position;
+    }
+
+    void LateUpdate()
+    {
+        float currentAngle = transform.eulerAngles.y;
+        float desiredAngle = player.transform.eulerAngles.y;
+        float angle = Mathf.LerpAngle(currentAngle, desiredAngle, Time.deltaTime * damping);
+
+        Quaternion rotation = Quaternion.Euler(0, angle, 0);
+        transform.position = player.transform.position - (rotation * offset);
+
+        transform.LookAt(player.transform);
+    }
+}
