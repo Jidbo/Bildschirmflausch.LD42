710f8ac5cce344388a109c8210ff8a6d778af27f
Jidbo <Jidbo@users.noreply.github.com>
2018-08-12 19:53:03 +0200

    add prefabs for pause menu

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    [SerializeField]
+    GameObject UI;
+
+    enum GameState {PAUSED, PLAYING};
+
+    GameState currentGameState = GameState.PLAYING;
+    bool pausedPressed = false;
+
+    public static GameController instance;
+    public GameController()
+    {
+        instance = this;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetAxis("Pause") > 0) {
+            if (currentGameState == GameState.PLAYING && !pausedPressed) {
+				UIController controller = UI.GetComponent<UIController>();
+                if (controller != null) {
+                    controller.showUI(true);
+                } else {
+                    Debug.Log("Cant find UI controller");
+                }
+
+            } else if (currentGameState == GameState.PAUSED && !pausedPressed) {
+                
+                UIController controller = UI.GetComponent<UIController>();
+                if (controller != null) {
+                    controller.showUI(false);
+                } else {
+                    Debug.Log("Cant find UI controller");
+                }
+            }
+
+			pausedPressed = true;
+        } else {
+            pausedPressed = false;
+        }
+	}
+
+    public void Pause() {
+        currentGameState = GameState.PAUSED;
+        Time.timeScale = 0;
+    }
+
+    public void Play() {
+        Time.timeScale = 1;
+        currentGameState = GameState.PLAYING;
+    }
+}
--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -2,15 +2,23 @@
 using UnityEngine.SceneManagement;
 
 public class UIController : MonoBehaviour {
-        
+
     public void QuitGame() {
         Debug.Log("Quit Game");
         Application.Quit();
     }
 
-
     public void LoadSceneByIndex(int index) {
         Debug.Log("Load Scene: " + index);
         SceneManager.LoadScene(index);
+    }
+
+    public void showUI(bool show) {
+        gameObject.SetActive(show);
+		if (show) {
+            GameController.instance.GetComponent<GameController>().Pause();
+		} else {
+            GameController.instance.GetComponent<GameController>().Play();
+		}
     }
 }
