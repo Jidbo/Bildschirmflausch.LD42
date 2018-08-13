c3a2e81a2e01458ac071223fd4a6de19b150b8b1
Chris <christian.seybert@stud.tu-darmstadt.de>
2018-08-13 16:11:52 +0200

    Add hazards

---
+++ Assets/Scripts/Obstacles/ScrapRoll.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrapRoll : MonoBehaviour {
+
+    [SerializeField]
+    protected float speed = 360.0f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(Vector3.forward * Time.deltaTime * speed);
+    }
+}
