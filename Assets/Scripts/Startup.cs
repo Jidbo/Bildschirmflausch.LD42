66456ae02e3aac94452b34941a0d20c57e9a6c55
Unknown <34157676+ALoTron@users.noreply.github.com>
2018-08-13 16:38:37 +0200

    Added Objects on startup.

--- Assets/Scripts/Startup.cs
+++ Assets/Scripts/Startup.cs
@@ -15,6 +15,17 @@ public class Startup : MonoBehaviour {
     private void Start() {
         GameObject newBigShelf01 = Instantiate(bigShelf);
         newBigShelf01.transform.position = new Vector3((long) 6.41811, (long) -2.861023e-06, (long) -5.716771);
-        newBigShelf01.transform.rotation = new Quaternion(0, (long) -53.662, 0, 0);
+        newBigShelf01.transform.Rotate(0, (float)-53.662, 0);
+
+        GameObject newBigShelf02 = Instantiate(bigShelf);
+        newBigShelf02.transform.position = new Vector3((long)4.466072, (long)1.506221, (long)6.428141);
+        newBigShelf02.transform.Rotate(0, (float)47.826, (float)-90.172);
+        
+
+        GameObject newBigShelf03 = Instantiate(bigShelf);
+        newBigShelf03.transform.position = new Vector3((long)-6.767082, (long)0.003471136, (long)0.7262506);
+        newBigShelf03.transform.Rotate(0, (float)-59.176, 0);
+        newBigShelf03.GetComponent<StorageBigShelf>().AddToStorage(Instantiate(waste));
+        newBigShelf03.GetComponent<StorageBigShelf>().AddToStorage(Instantiate(waste));
     }
 }
