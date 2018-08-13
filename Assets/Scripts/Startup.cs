f91c9112b62a7b9a16edcec90d981691629d250a
Triceraptodactyl <reispfannenfresser@gmail.com>
2018-08-13 15:55:51 +0200

    Moved Spawning-Code to Start()

--- Assets/Scripts/Startup.cs
+++ Assets/Scripts/Startup.cs
@@ -4,8 +4,7 @@ using UnityEngine;
 using UnityEditor;
 
 [InitializeOnLoad]
-public class Startup : MonoBehaviour
-{
+public class Startup : MonoBehaviour {
     [SerializeField]
     GameObject bigShelf;
     [SerializeField]
@@ -13,8 +12,7 @@ public class Startup : MonoBehaviour
     [SerializeField]
     GameObject waste;
 
-    public Startup()
-    {
+    private void Start() {
         GameObject newBigShelf01 = Instantiate(bigShelf);
         newBigShelf01.transform.position = new Vector3((long) 6.41811, (long) -2.861023e-06, (long) -5.716771);
         newBigShelf01.transform.rotation = new Quaternion(0, (long) -53.662, 0, 0);
