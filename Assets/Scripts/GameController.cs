7a5448054119bdc772d49cef28905de21e5a1f94
Jidbo <Jidbo@users.noreply.github.com>
2018-08-13 14:24:24 +0200

    add score

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,11 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour {
 
     [SerializeField]
     GameObject UI;
+    [SerializeField]
+    GameObject ScoreText;
+
+    float lastTime = 0;
+    float gameScore = 0;
 
     enum GameState {PAUSED, PLAYING};
 
@@ -17,9 +23,14 @@ public class GameController : MonoBehaviour {
     {
         instance = this;
     }
-	
+
+	private void Start() {
+        lastTime = Time.time;
+	}
+
 	// Update is called once per frame
 	void Update () {
+
         if (Input.GetAxis("Pause") > 0) {
             if (currentGameState == GameState.PLAYING && !pausedPressed) {
 				UIController controller = UI.GetComponent<UIController>();
@@ -53,5 +64,14 @@ public class GameController : MonoBehaviour {
     public void Play() {
         Time.timeScale = 1;
         currentGameState = GameState.PLAYING;
+    }
+
+    public void updateScore(float toAdd) {
+        gameScore += toAdd;
+        ScoreText.GetComponent<Text>().text = gameScore.ToString();
+    }
+
+    public float getCurrentScore() {
+        return gameScore;
     }
 }
